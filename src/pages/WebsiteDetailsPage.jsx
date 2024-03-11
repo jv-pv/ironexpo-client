db2733ee21fb64a410f71d9b538bec4b3fe92cae
John Pieri <jvpieri@gmail.com>
2024-03-10 21:13:27 -0400

    Added fake web browser

--- src/pages/WebsiteDetailsPage.jsx
+++ src/pages/WebsiteDetailsPage.jsx
@@ -1,9 +1,51 @@
-import React from 'react'
+import { useContext, useEffect } from "react";
+import { WebsiteContext } from "../context/website.context";
+import { useParams } from 'react-router-dom'
+
 
 const WebsiteDetailsPage = () => {
-  return (
-    <div>WebsiteDetailsPage</div>
-  )
-}
 
-export default WebsiteDetailsPage
+  const { selectedWebsite, fetchWebsiteDetails } = useContext(WebsiteContext)
+
+  const { websiteId } = useParams()
+
+  useEffect(() => {
+    fetchWebsiteDetails(websiteId)
+  }, [websiteId])
+  
+  return (
+    <div className="flex flex-col justify-center items-center w-full h-full flex-1 p-4">
+      <div className="container">
+        <div className="browser-nav">
+
+          <div className="dots">
+
+            <span className="dot"></span>
+            <span className="dot"></span>
+            <span className="dot"></span>
+
+          </div>
+
+          <div className="url-bar">
+            <input type="text" defaultValue={selectedWebsite?.url}/>
+          </div>
+
+
+        </div>
+
+          {/* <div className="iframe-container">
+            <iframe src={selectedWebsite?.url} frameborder="0"></iframe>
+          </div> */}
+
+      </div>
+
+
+      <div className="description">
+        <p>{selectedWebsite?.description}</p>
+      </div>
+
+    </div>
+  );
+};
+
+export default WebsiteDetailsPage;
