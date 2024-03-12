ebf7f6c00393600953df1a8288fda7e34103c1ac
John Pieri <jvpieri@gmail.com>
2024-03-12 14:04:43 -0400

    Refactored card and form styling.

--- src/components/WebsiteCard.jsx
+++ src/components/WebsiteCard.jsx
@@ -2,17 +2,17 @@ import { NavLink } from "react-router-dom"
 
 const WebsiteCard = ({website}) => {
   return (
-    <div className='card flex flex-col w-96 h-auto max-h-[550px] min-h-[550px] bg-gray-100 rounded-xl justify-self-center p-2'>
-        <div className='card-image'>
+    <div className="card flex flex-col w-96 h-auto max-h-[570px] min-h-[570px] bg-gray-100 rounded-xl justify-self-center p-2">
+        <div className="card-image">
             <iframe src={website.url}></iframe>
         </div>
         <div className="card-link text-center text-blue-500 text-base p-2 block">
             <a href={website.url}>{website.url}</a>
         </div>
-        <p className='card-description flex items-center p-2 text-sm flex-1'>
+        <p className="card-description p-2 text-sm flex-1">
             {website.description}
         </p>
-        <div className='tech-stack flex p-2 gap-2'>
+        <div className="flex flex-wrap w-full p-2 gap-2">
             {website.technologies.map((tech, index) => (
                 <span className="tech-tag" key={index}>{tech}</span>
             ))
@@ -23,7 +23,7 @@ const WebsiteCard = ({website}) => {
             <NavLink to={`/websites/${website.id}`}>
                 <button className="bg-blue-500 text-blue-100 px-2 rounded">...</button>
             </NavLink>
-            <p className='published-date text-xs text-right'>Published: {website.publishDate}</p>
+            <p className="published-date text-xs text-right">Published: {website.publishDate}</p>
         </div>
     </div>
   )
