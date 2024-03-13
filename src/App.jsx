a1351da3a187f7a8926b9566714797c6d6282f6f
John Pieri <jvpieri@gmail.com>
2024-03-13 19:20:49 -0400

    Removed AboutPage component and it's link.

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,6 @@
 import './App.css'
 import { Routes, Route } from 'react-router-dom'
-import Navbar from './components/Navbar'
 import HomePage from './pages/HomePage'
-import AboutPage from './pages/AboutPage'
 import WebsitesPage from './pages/WebsitesPage'
 import PublishPage from './pages/PublishPage'
 import WebsiteDetailsPage from './pages/WebsiteDetailsPage'
@@ -10,13 +8,12 @@ import EditWebsiteDetailsPage from './pages/EditWebsiteDetailsPage.jsx'
 
 const App = () => {
   return (
-    <div className="flex flex-col bg-blue-100 w-full min-h-dvh">
+    <div className="flex flex-col bg-blue-950 w-full min-h-dvh">
 
       <Routes>
 
         
         <Route path='/' element={<HomePage />}/>
-        <Route path='/about' element={<AboutPage />}/>
         <Route path='/websites' element={<WebsitesPage />}/>
         <Route exact path='/websites/publish' element={<PublishPage />}/>
         <Route path='/websites/:websiteId' element={<WebsiteDetailsPage />}/>
