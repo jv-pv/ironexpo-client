36de27ca251067fc743b7a924406781c5a3efa51
John Pieri <jvpieri@gmail.com>
2024-03-14 15:15:38 -0400

    Replaced WebisteCard component with this simplified card.

---
+++ src/components/WebsiteCardFull.jsx
@@ -0,0 +1,22 @@
+import { NavLink } from "react-router-dom"
+
+const WebsiteCardFull = ({website}) => (
+
+    <NavLink to={`/websites/${website.id}`}>
+        <div className="aspect-square w-full hover:scale-75 transition-transform duration-300">
+
+        <iframe 
+            title={`Preview of ${website.url}`}
+            src={website.url} 
+            loading="lazy"
+            className="aspect-square w-full pointer-events-none select-none"
+        />
+
+        </div>
+    </NavLink>
+  )
+
+export default WebsiteCardFull
+
+
+// ? Keep in that using index as a key can cause problems when editing the card data in the form.
