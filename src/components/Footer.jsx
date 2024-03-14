eeb8350c16fcba2855e5d674e457b378930b448d
John Pieri <jvpieri@gmail.com>
2024-03-14 15:12:42 -0400

    Added a simplified header component and added a footer component.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,9 @@
 
 const Footer = () => {
   return (
-    <div className="flex items-center justify-between px-2 w-full bg-blue-900">
-        <p className="text-blue-50 font-heading">Made by John</p>
-        <p className="text-blue-50 font-heading">IronHack 2024</p>
+    <div className="flex items-center justify-between px-2 w-full">
+        <p className="text-blue-50">Made by John</p>
+        <p className="text-blue-50">IronHack 2024</p>
     </div>
   )
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,19 @@
+const Header = () => {
+
+  return (
+      <>
+        <span className="fixed left-[20px] top-[24px] text-[62px] text-justify text-white leading-tight z-20 mix-blend-difference">IronExpo</span>,
+        <p className="text-[62px] indent-[270px] text-justify text-white mb-5 mx-5 leading-tight">
+          where IronHack's talent shines. Explore innovative projects, get inspired, and share your creations with a vibrant community eager to see what you've built. IronExpo is your stage for discovery, inspiration, and connection. Join us and let your creativity soar!
+        </p>
+      </>
+  );
+};
+
+export default Header;
+
+// text-indent: 50px;
+
+{/* <p className="text-[62px] text-justify text-white mb-5 mx-5 leading-tight">
+<span className="opacity-60">IronExpo</span>, where IronHack's talent shines. Explore innovative projects, get inspired, and share your creations with a vibrant community eager to see what you've built. IronExpo is your stage for discovery, inspiration, and connection. Join us and let your creativity soar!
+</p> */}
