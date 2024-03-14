403c229ae1581d6a5d698bca39ac0c720bf6e9b9
John Pieri <jvpieri@gmail.com>
2024-03-14 15:18:36 -0400

    Removed page header, integrated this component to the homepage for easier navigation and ux.

--- src/pages/WebsitesPage.jsx
+++ src/pages/WebsitesPage.jsx
@@ -1,7 +1,7 @@
-import { useEffect, useState, useContext } from 'react'
+import { useEffect, useContext } from 'react'
 import WebsiteCard from '../components/WebsiteCard'
 import { WebsiteContext } from '../context/website.context'
-import Navbar from '../components/Navbar'
+import WebsiteCardFull from '../components/WebsiteCardFull'
 
 const WebsitesPage = () => {
 
@@ -14,24 +14,18 @@ const WebsitesPage = () => {
 
   return (
     <>
-    <Navbar/>
-    <div className='flex flex-col w-full mx-auto max-w-[1600px] h-full flex-1 mb-6'>
-        <div className='p-2 my-5'>
-            <h2 className='font-heading text-6xl text-blue-50 mx-4 my-8 text-center'>Explore Published Sites</h2>
-        </div>
 
-
-        <div className='website-grid'>
+        <div className='website-grid mx-3 my-2'>
 
             {websites.map((website) => (
-                <WebsiteCard key={website.id} website={website}/>
+                <WebsiteCardFull key={website.id} website={website}/>
+                // <WebsiteCard key={website.id} website={website}/>
             ))}
 
         </div>
 
 
 
-    </div>
 
     </>
   )
