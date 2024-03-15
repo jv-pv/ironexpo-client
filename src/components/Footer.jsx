4a09df4fff852f0cccdb23ad37656e1c729e8ef7
John Pieri <jvpieri@gmail.com>
2024-03-15 13:19:19 -0400

    Added github lnk to footer element.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,11 @@ const Footer = () => {
   return (
     <div className="flex items-center justify-between px-3 w-full h-8">
         <p className="text-blue-50">
-          <span className="text-white">John</span> ✷ <span className="text-lime-400">Pieri</span>
+          <a href="https://github.com/jv-pv" target="_blank" className="flex gap-1">
+            <span className="text-white">John</span> 
+                <img src="/github-circle.svg" alt="github-icon" className="w-5 h-5 self-center"/>
+             <span className="text-lime-400">Pieri</span>
+          </a>
         </p>
         <p className="text-blue-50">
           <span className="text-blue-400">IronHack</span> ✷ <span className="text-">2024</span>
