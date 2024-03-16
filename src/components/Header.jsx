9e9adb61538bc31ddad29e7ccc5e7b6f6532e1c7
John Pieri <jvpieri@gmail.com>
2024-03-15 20:08:58 -0400

    Added z-20 to header span element to place it over the webpage content.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ const Header = () => {
 
   return (
       <>
-        <span className="fixed left-[20px] top-[24px] text-[62px] text-justify text-gray-50 leading-tight z-10 mix-blend-difference text-shadow">IronExpo</span>,
+        <span className="fixed left-[20px] top-[24px] text-[62px] text-justify text-gray-50 leading-tight z-20 mix-blend-difference text-shadow">IronExpo</span>,
         <p className="text-[62px] indent-[270px] text-justify text-gray-300 mb-5 mx-5 leading-tight text-shadow z-10">
           where IronHack's talent shines. Explore innovative projects, get inspired, and share your creations with a vibrant community eager to see what you've built. IronExpo is your stage for discovery, inspiration, and connection. Join us and let your creativity soar!
         </p>
